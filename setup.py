630b04d47dd6febef02e2dd6767f37d110de688c
Daniel Waeber <daniel.waeber@gameduell.de>
2014-06-22 23:00:54 +0200

    simple setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zeroflo",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = [],
+    install_requires = [
+            'docutils>=0.3',
+            'aiozmq>=0.4',
+            ],
+    package_data = {
+        '': ['*.txt', '*.rst'],
+    },
+
+    # metadata for upload to PyPI
+    author = "wabu",
+    author_email = "wabu@fooserv.net",
+    description = "Øflo is a flow based programming framework for python using ØMQ",
+    license = "MIT",
+    keywords = "flow fbp ØMQ pipes processing",
+    url = "https://github.com/wabu/zeroflo",
+)
