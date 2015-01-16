342cc49657c8e340578462c811a10b7e21a256b9
wabu <wabu@fooserv.net>
2015-01-16 13:34:12 +0100

    experimental top without ids

--- zeroflo/top/topology.py
+++ zeroflo/top/topology.py
@@ -1,120 +1,177 @@
-from .container import (Container,
-                        ReferedContainer,
-                        AssocContainer,
-                        SetsContainer,
-                        GroupedContainer)
-
 from collections import namedtuple
 
-Link = namedtuple('Link', 'sid, tid')
-
-
-class Topology:
+class Units:
     def __init__(self):
-        self.units = Container('unit')
-
-        self.tgtports = ReferedContainer('in')
-        self.srcports = ReferedContainer('out')
-
-        self.links = AssocContainer('link')
-
-        self.bundles = SetsContainer('bundle')
-        self.bundle_opts = {}
-        self.spaces = GroupedContainer('space')
+        self.units = set()
 
     def register(self, unit):
         return self.units.add(unit)
 
     def unregister(self, unit):
-        uid = self.units.index(unit)
-
-        for sid in self.srcports.refered_keys(uid):
-            for link in self.links.refered_values(sid=sid):
-                self.unlink(self.srcports[sid], self.tgtports[link.tid])
-            del self.srcports[sid]
-
-        for tid in self.tgtports.refered_keys(uid):
-            for link in self.links.refered_values(tid=tid):
-                self.unlink(self.srcports[link.sid], self.tgtports[tid])
-            del self.tgtports[tid]
-
-        for bid in list(self.bundles.refered_ids(uid)):
-            bdl = self.bundles.pop(bid)
-            opts = self.bundle_opts.pop(bid)
-            bdl.remove(uid)
-            if bdl:
-                self.bundle(bdl, **opts)
-
-        # TODO broadcast as we use container methods
-        self.bundles.dismiss(uid)
-        self.spaces.dismiss(uid)
-
         self.units.remove(unit)
 
+
+def remove_from(dct, key, item):
+    entry = dct[key]
+    entry.remove(item)
+    if not entry:
+        del dct[key]
+
+
+class Link:
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+
+
+class Links(Units):
+    def __init__(self):
+        self._links = set()
+        self._source_refs = defaultdict(list)
+        self._target_refs = defaultdict(list)
+
     def link(self, source, target):
-        sid = self.srcports.add(source, ref=self.units.add(source.unit))
-        tid = self.tgtports.add(target, ref=self.units.add(target.unit))
-        link = Link(sid, tid)
-        self.links.add(link)
+        link = Link(source, target)
+        self._links.add(link)
+
+        self._source_refs[source.unit].add(link)
+        self._target_refs[target.unit].add(link)
         return link
 
-    def unlink(self, source, target):
-        sid = self.srcports.add(source, ref=self.units.index(source.unit))
-        tid = self.tgtports.add(target, ref=self.units.index(target.unit))
-        link = Link(sid, tid)
-        self.links.remove(link)
+    def links_from(self, source):
+        try:
+            source = source.unit
+        except AttributeError:
+            pass
+        return self._source_refs[source]
 
-    def bundle(self, units, **opts):
-        uids = {self.units.add(u) for u in units}
-        ref = self.bundles.add(uids)
+    def links_to(self, target):
+        try:
+            target = target.unit
+        except AttributeError:
+            pass
+        return self._target_refs[target]
 
-        bdl_opts = self.bundle_opts.setdefault(ref.id, {})
-        self._bdl_update(units, bdl_opts, opts)
-        return ref
+    def unlink(self, link):
+        remove_from(self._source_refs, link.source.unit, link)
+        remove_from(self._target_refs, link.target.unit, link)
+        self._links.remove(link)
 
-    def _bdl_update(self, units, bdl, opts):
-        conflicts = [key
-                     for key, val in opts.items()
-                     if key in bdl and bdl[key] != val]
-        if conflicts:
-            raise IndexError('conflicting values for {} of bundle {}'.format(
-                ', '.join(conflicts), set(units)))
-        bdl.update(opts)
+    def unregister(self, unit):
+        links = self._source_refs[unit] + self._target_refs[unit]
+        for link in links:
+            self.unlink(link)
+        super().unregister(unit)
 
-    def unbundle(self, units):
-        uids = {self.units.index(u) for u in units}
-        ref = self.bundles.remove(uids)
-        return self.bundle_opts.pop(ref.id)
 
-    def join(self, *units):
-        uids = {self.units.add(u) for u in units}
-        self.spaces.join(*uids)
+class Space:
+    def __init__(self, units=set()):
+        self.units = units
 
-    def part(self, *units):
-        uids = {self.units.add(u) for u in units}
-        self.spaces.part(*uids)
 
-    def __repr__(self):
-        its = []
-        for sid, uids in self.spaces.items():
-            its.append('{!r}:'.format(sid))
-            for uid in uids:
-                unit = self.units[uid]
-                its.append('  {!r} ({!r}):'.format(unit, uid))
+class Spaces(Units):
+    def __init__(self):
+        # {space}
+        self._spaces = set()
 
-                for tid, target in self.tgtports.refered_items(uid):
-                    for sid, _ in self.links.refered_values(tid=tid):
-                        source = self.srcports[sid]
-                        its.append('    {} >> {}'.format(source, target))
+        # {unit: space}
+        self._refs = {}
 
-                for sid, source in self.srcports.refered_items(uid):
-                    for _, tid in self.links.refered_values(sid=sid):
-                        target = self.tgtports[tid]
-                        its.append('    {} >> {}'.format(source, target))
+        # {space: {space}}
+        self._pars = defaultdict(set)
 
-        its.append('')
-        for bid, bundle in self.bundles.items():
-            opts = self.bundle_opts[bid.id]
-            its.append('{}:\n    {}'.format(bundle, opts))
+    def space_of(unit):
+        return self.refs.get(unit)
 
-        return '\n'.join(its)
+    def join(self, units):
+        # spaces that may get joined
+        spaces = set(filter(None, map(self.space_of, units)))
+        # pars of these spaces
+        pars = set.union(self._pars[space] for space in spaces)
+
+        if len(pars.intersection(spaces)) > 1:
+            raise IndexError('Cannot join units that were pared')
+
+        # get new primary space
+        others = []
+        if len(spaces) == 0:
+            space = Space()
+            self._spaces.add(space)
+        elif len(spaces) == 1:
+            space, = spaces
+        else:
+            space,*others = spaces
+
+        # primary space gets new and others units and pars
+        space.units.update(units)
+        self._pars[space] = pars
+        for other in others:
+            space.units.update(others)
+
+        # remove par references to others but par the new space
+        for par in pars:
+            self._pars[par].difference_update(others)
+            self._pars[par].add(space)
+
+        return space
+
+
+    def par(self, units):
+        for u in units:
+            space = self.space_of(unit)
+            if space:
+                if len(space.units.intersection(units)) > 1:
+                    raise IndexError('Cannot par joined units')
+
+        spaces = {self.space_of(unit) or self.join({u}) for u in units}
+        for space in spaces:
+            self._parts[space] = spaces
+
+    def unregister(self, unit):
+        space = self._refs.pop(unit)
+        space.units.remove(unit)
+
+        if not space:
+            pars = self._pars.pop(space)
+            for par in pars:
+                self._pars[par].remove(space)
+        super().unregister(unit)
+
+
+class Bundle:
+    def __init__(self, units=set(), opts={}):
+        self.units = units
+        self.opts = opts
+
+    def update(self, opts):
+        conflict = {opt for opt in opts
+                        if opt in self.opts and self.opts[opt] != opts[opt]}
+
+        if conflict:
+            raise IndexError('Options {} already set for bundle {}'.format(
+                conflict, self))
+
+        self.opts.update(opts)
+
+
+class Bundles(Units):
+    def __init__(self):
+        self._bunldes = defaultdict(list)
+
+    def bunlde(self, units, **opts):
+        bundle = Bundle(units, opts)
+
+        for u in units:
+            self._bundles[u].append(bundle)
+
+        return bundle
+
+    def unregister(self, unit):
+        for u, bdl in self._bunldes.items():
+            bdl.remove(unit)
+            if not bdl:
+                del self._bundles[u]
+        super().unregister(unit)
+
+
+class Topology(Links, Spaces, Bundles):
